1c02f138221370c2feede2d54835585914c412fa
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-29 02:57:03 +0500

    parting up

---
+++ src/.vuepress/locales.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "/": {
+        title: "BIS | Chisel Devs",
+        description: "BSc (Hons) in Business Information Systems",
+    },
+    "/csf/": {
+        title: "CSF | BIS",
+        description: "Computer Science Fundamentals | BIS",
+    },
+    "/fop/": {
+        title: "FOP | BIS",
+        description: "Fundamentals of Programming | BIS",
+    },
+    "/imob/": {
+        title: "IMOB | BIS",
+        description:
+            "Introduction to Management and Organisational Behavior | BIS",
+    },
+    "/isds/": {
+        title: "ISDS | BIS",
+        description: "Introduction to Statistics and Data Science | BIS",
+    },
+    "/wt/": {
+        title: "WT | BIS",
+        description: "Web Technology | BIS",
+    },
+}
