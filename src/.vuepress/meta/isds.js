435785bfba1ccf528dba096a7c9ac2e3c3d69832
Sokhibjon Orzikulov <genemators@gmail.com>
2020-09-29 17:18:53 +0500

    Statistics getting started

--- src/.vuepress/meta/isds.js
+++ src/.vuepress/meta/isds.js
@@ -10,5 +10,49 @@ module.exports = {
       text: "Main",
       link: "/isds/",
     },
+    {
+      text: "Overview",
+      link: "/isds/overview/",
+    },
+    {
+      text: "Useful",
+      link: "/isds/useful/",
+    },
+    {
+      text: "Teaching Weeks",
+      ariaLabel: "Weeks",
+      items: [
+        {
+          text: "Teaching Week 1",
+          items: [
+            {
+              text: "Lecture",
+              link: "/isds/tw1/lecture/",
+            },
+            {
+              text: "Seminar",
+              link: "/isds/tw1/seminar/",
+            },
+          ],
+        },
+      ],
+    },
   ],
+  sidebar: {
+    "/": [
+      {
+        title: "Teaching Week 1",
+        collapsable: true,
+        children: [
+          "/isds/tw1/lecture.md",
+          "/isds/tw1/seminar.md",
+          {
+            text: "Quiz",
+            link:
+              "https://intranet.wiut.uz/Questionnaire/AnswerQuestionnaire?ID=531&moduleID=0&way=lm",
+          },
+        ],
+      },
+    ],
+  },
 };
